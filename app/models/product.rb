7ddb25989d2861692ee4b698780795f46eb66429
smith <smith@futueworkz.com>
2015-05-21 13:23:12 +0700

    Write test case for description of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,25 @@
 class Product < ActiveRecord::Base
-	validates :title, :description, presence: true
+	# extend Enumerize
+	validates :title, :description, :category, presence: true
 	validates :price, presence: true, numericality: {greater_than: 0}
+	validate :title_is_shorter_than_description
 
 	belongs_to :category
+
+	before_validation :strip_html_from_description, :make_title_lowercase
+
+	def strip_html_from_description
+		self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+	end
+
+	def make_title_lowercase
+		self.title = self.title.downcase unless self.title.nil?
+	end
 	
+	def title_is_shorter_than_description
+		return if title.blank? or description.blank?
+		if description.length < title.length
+			errors.add(:description, 'can\'t be shorter than title')
+		end
+	end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-	it { should validate_presence_of :title }
-	it { should validate_presence_of :description}
-	it { should validate_presence_of :price}
-	it { should validate_numericality_of(:price).is_greater_than(0) }
-	it { should belong_to(:category)}
+	context 'Validation' do
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :description}
+		it { should validate_presence_of :price}
+		it { should validate_numericality_of(:price).is_greater_than(0) }
+	end
+
+	context 'Association' do
+		it { should belong_to(:category)}
+	end
+
+	context '#strip_html_description' do
+		let!(:product) do
+			Product.new(description: '<h1>ruby book</h1>')
+		end 
+
+		it 'strips HTML from description before validating' do
+			product.validate
+			expect(product.description).to eq 'ruby book'
+		end
+	end
+
+	describe '#make_title_lowercase' do
+		let!(:product) { Product.new(title: 'RUBY') }
+
+		it 'make the title lower before validating' do
+			product.validate
+			expect(product.title).to eq 'ruby'
+		end
+	end
+
+	context '#title_is_shorter_than_description' do
+		let!(:product) { Product.new(title: 'this is a ruby book', description: 'ruby book', price: 12)}
+
+		it 'title is shorter than description' do
+			product.validate
+			expect(product.errors.messages).to include(description: ["can't be shorter than title"])
+		end
+	end
 end
